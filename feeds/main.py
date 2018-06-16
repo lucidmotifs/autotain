fb52d75d771716053603aa45af295de00bcc7fcd
Paul Cooper <paulcooper@linux-lnc5.fritz.box>
2018-06-17 00:41:34 +0200

    feeds (showrss) module initial

---
+++ feeds/main.py
@@ -0,0 +1,23 @@
+""" showrss feed parser task. invoked when a feed has new items. builds 'show'
+    objects with a magnet links attached. these objects are pickled and added to
+    a file queue. new queue items message is returned to autotain daemon informing
+    it that transfer task should be invoked. """
+import argparse
+import logging
+import feedparser
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "feed", help="create new download items from rss feed", type=str)
+args = parser.parse_args()
+
+def main():
+    feed = feedparser.parse(args.feed)
+    for e in feed.entries:
+        print(e['title'])
+        print(e['link'])
+        print('\n')
+
+
+if __name__ == '__main__':
+    main()
